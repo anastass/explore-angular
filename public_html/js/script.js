e01f64b3252476be281550d7f8bd3852bd6419c0
Alexandre Anastassov <a.anastassov@iaea.org>
2015-11-13 15:39:39 +0100

    Use of $http services

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,10 +1,20 @@
 (function () {
     var app = angular.module("app", []);
 
-    var MainController = function ($scope) {
-        $scope.message = "Hello world!";
+    var MainController = function ($scope, $http) {
+
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+        };
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the user";
+        };
+
+        $http.get("https://api.github.com/users/jgarcial")
+                .then(onUserComplete, onError);
     };
 
-    app.controller("MainController", ["$scope", MainController]);
+    app.controller("MainController", ["$scope", "$http", MainController]);
 }());
 
